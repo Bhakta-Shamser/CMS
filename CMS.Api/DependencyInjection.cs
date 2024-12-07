43afd8c8941a258fad1304d9ee8501b5516897f0
shdha092 <shamser.dhami@javra.com>
2024-12-07 22:52:24 +0545

    Candidate Management System (CMS) project setup, Dependency Injection implemented and peoject refrence added.

---
+++ CMS.Api/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using CMS.Application;
+using CMS.Infrastructure;
+using CMS.Contract;
+
+namespace CMS.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiDI(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddApplication()
+                .AddInfrastructure()
+                .AddContract();
+            
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+            return services;
+        }
+    }
+}
