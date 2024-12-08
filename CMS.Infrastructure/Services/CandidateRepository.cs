46b8a4dc9fc17d43405020c8b4f4b406169afbef
shdha092 <shamser.dhami@javra.com>
2024-12-08 17:50:14 +0545

    un-used namespaces removed.

--- CMS.Infrastructure/Services/CandidateRepository.cs
+++ CMS.Infrastructure/Services/CandidateRepository.cs
@@ -1,11 +1,6 @@
 ﻿using CMS.Contract.Interfaces;
 using CMS.Domain.Entities;
 using CMS.Infrastructure.Persistence;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CMS.Infrastructure.Services
 {
