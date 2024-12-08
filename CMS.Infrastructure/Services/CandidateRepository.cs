57c49a28f1fc328dd1de5486321e4eb65476da75
shdha092 <shamser.dhami@javra.com>
2024-12-08 13:03:16 +0545

    CandidateRepository

---
+++ CMS.Infrastructure/Services/CandidateRepository.cs
@@ -0,0 +1,43 @@
+﻿using CMS.Contract.Interfaces;
+using CMS.Domain.Entities;
+using CMS.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Infrastructure.Services
+{
+    public class CandidateRepository : BaseRepository, ICandidateRepository, ICandidateReadRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+
+        public CandidateRepository(ISqlConnectionFactory connectionFactory, ApplicationDbContext dbContext)
+             : base(connectionFactory)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddAsync(Candidate candidate, CancellationToken cancellationToken)
+        {
+            await _dbContext.Candidates.AddAsync(candidate, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+        public async Task<Candidate?> GetByEmailSqlAsync(string email, CancellationToken cancellationToken)
+        {
+            var query = "SELECT * FROM Candidates WHERE Email = @Email";
+            var parameters = new { Email = email };
+            return await ExecuteQuerySingleAsync<Candidate>(query, parameters, cancellationToken);
+        }
+
+        public async Task UpdateAsync(Candidate candidate, CancellationToken cancellationToken)
+        {
+            _dbContext.Candidates.Update(candidate);
+           
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+    }
+}
