39c7064b6ae212549cf199ad4ae769a70bb0ae36
shdha092 <shamser.dhami@javra.com>
2024-12-08 12:51:34 +0545

    SQLConnectionFactory and BaseRepo created.

---
+++ CMS.Infrastructure/Services/BaseRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using Dapper;
+
+namespace CMS.Contract.Interfaces
+{
+    public abstract class BaseRepository
+    {
+        private readonly ISqlConnectionFactory _connectionFactory;
+
+        protected BaseRepository(ISqlConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+        protected async Task<IEnumerable<T>> ExecuteQueryAsync<T>(string query, object parameters = null, CancellationToken cancellationToken = default)
+        {
+            using (var connection = await _connectionFactory.CreateConnectionAsync(cancellationToken))
+            {
+                return await connection.QueryAsync<T>(query, parameters, commandType: CommandType.Text, commandTimeout: 30);
+            }
+        }
+
+        protected async Task<T> ExecuteQuerySingleAsync<T>(string query, object parameters = null, CancellationToken cancellationToken = default)
+        {
+            using (var connection = await _connectionFactory.CreateConnectionAsync(cancellationToken))
+            {
+                return await connection.QuerySingleOrDefaultAsync<T>(query, parameters, commandType: CommandType.Text, commandTimeout: 30);
+            }
+        }
+    }
+}
