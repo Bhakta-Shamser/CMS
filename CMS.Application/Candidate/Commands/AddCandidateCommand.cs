f26ffbd51a60d6e2b5f431ef0780eba8e9142f62
shdha092 <shamser.dhami@javra.com>
2024-12-08 13:18:59 +0545

    Candidate Commands

---
+++ CMS.Application/Candidate/Commands/AddCandidateCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace CMS.Application.Candidate.Commands
+{
+    public sealed record AddCandidateCommand(
+     string Email,
+     string FirstName,
+     string LastName,
+     string? PhoneNumber,
+     DateTime? AvailableFrom,
+     DateTime? AvailableTo,
+     string? LinkedInProfileUrl,
+     string? GithubProfileUrl,
+     string Comment
+     ) : IRequest<string>;
+}
---
+++ CMS.Application/Candidate/Commands/AddCandidateCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using CMS.Contract.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Application.Candidate.Commands
+{
+    public class AddCandidateCommandHandler : IRequestHandler<AddCandidateCommand, string>
+    {
+        private readonly ICandidateRepository _repository;
+        public AddCandidateCommandHandler(ICandidateRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<string> Handle(AddCandidateCommand request, CancellationToken cancellationToken)
+        {
+            var candidate = new CMS.Domain.Entities.Candidate(
+                request.Email,
+                request.FirstName,
+                request.LastName,
+                request.PhoneNumber,
+                null,
+                request.AvailableFrom,
+                request.AvailableTo,
+                request.LinkedInProfileUrl,
+                request.GithubProfileUrl,
+                request.Comment
+            );
+
+            await _repository.AddAsync(candidate, cancellationToken);
+            return candidate.Email;
+        }
+    }
+}
